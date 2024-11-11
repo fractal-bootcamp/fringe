e16f6f3b0db67e4bf5b71daadc6258b0e5b413e3
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-11 13:41:28 -0500

    Added base preferences page

---
+++ src/app/preferences/page.tsx
@@ -0,0 +1,11 @@
+import Preferences from "@/components/Preferences";
+
+const Page = () => {
+  return (
+    <div>
+      <Preferences />
+    </div>
+  );
+};
+
+export default Page;
--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -1,3 +1,6 @@
+import { faGears } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface ProfilePageProps {
   name: string;
   profilePhoto: string;
@@ -6,6 +9,13 @@ interface ProfilePageProps {
 const ProfilePage = ({ name }: ProfilePageProps) => {
   return (
     <div className="flex flex-col items-center justify-center">
+      <div className="w-full flex justify-between items-center">
+        <div>Fringe +</div>
+        <button className="flex space-x-2">
+          <p className="text-xs">Preferences</p>
+          <FontAwesomeIcon icon={faGears} />
+        </button>
+      </div>
       <div className="rounded-full w-20 h-20 bg-sky-200"></div>
       <p>{name}</p>
     </div>
