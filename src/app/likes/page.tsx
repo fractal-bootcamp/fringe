621ea37b37ab640845c16d970bf56bb95c6d6c2e
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-11 12:22:02 -0500

    Applied global store for likes you page

--- src/app/likes/page.tsx
+++ src/app/likes/page.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import { dummyApplicants } from "@/api/dummyApplicants";
+import { dummyCompanies } from "@/api/dummyCompanies";
 import LikesYouPage from "@/components/LikesYouPage";
+import { userTypeStore } from "@/stores/userTypeStore";
 
 const Page = () => {
+  const { userType } = userTypeStore();
   const likesYouApplicants = dummyApplicants.map((applicant) => ({
     name: applicant.name,
     profilePhoto: applicant.profilePhotoIds[0],
   }));
+
+  const likesYouCompanies = dummyCompanies.map((company) => ({
+    name: company.name,
+    profilePhoto: company.profilePhotoIds[0],
+  }));
+
+  const likesYouUsers = userType === "applicant" ? likesYouCompanies : likesYouApplicants;
+
   return (
     <div>
-      <LikesYouPage likesYouUsers={likesYouApplicants} />
+      <LikesYouPage likesYouUsers={likesYouUsers} />
     </div>
   );
 };
