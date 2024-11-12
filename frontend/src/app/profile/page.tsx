ca40d85b4cbca9e81fc3ab5b5ba3951a0ab3bd71
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-12 12:16:30 -0500

    Added useUser hook

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import { dummyApplicants } from "@/api/dummyApplicants";
-import { dummyCompanies } from "@/api/dummyCompanies";
 import ProfilePage from "@/components/ProfilePage";
-import { userTypeStore } from "@/stores/userTypeStore";
+import useUser from "@/hooks/useUser";
 import React from "react";
 
 const Page = () => {
-  const { userType } = userTypeStore();
-  const applicantProfile = {
-    name: dummyApplicants[0].name,
-    profilePhoto: dummyApplicants[0].profilePhotoIds[0],
-  };
-  const companyProfile = {
-    name: dummyCompanies[0].name,
-    profilePhoto: dummyCompanies[0].profilePhotoIds[0],
+  const { user } = useUser();
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  const userProfile = {
+    name: user.name,
+    profilePhoto: user.profilePhotoIds[0],
   };
 
-  const userProfile = userType === "applicant" ? applicantProfile : companyProfile;
   return (
     <div className="p-2">
       <ProfilePage name={userProfile.name} profilePhoto={userProfile.profilePhoto} />
---
+++ frontend/src/hooks/useUser.ts
@@ -0,0 +1,20 @@
+import { getUserById } from "@/api/apiUsers";
+import { User } from "@/types/types";
+import { useEffect, useState } from "react";
+
+const useUser = () => {
+  const userId = "1"; // TODO: get user id from backend
+  const [user, setUser] = useState<User>();
+  const fetchUser = async () => {
+    const user = await getUserById(userId);
+    setUser(user);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return { user };
+};
+
+export default useUser;
