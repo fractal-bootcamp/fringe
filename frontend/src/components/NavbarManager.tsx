b1a0fa36e56078287c480a741a1dd54028c29bdd
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-14 13:49:59 -0500

    Migrated storybook navbar to real app

--- frontend/src/components/NavbarManager.tsx
+++ frontend/src/components/NavbarManager.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-import Navbar from './Navbar';
-import { dataNavigation } from "@/utils/dataNavigation";
 import { useAuthContext } from "@/contexts/AuthContext";
+import XNavbar from "./XNavbar";
+import { UserButton } from "@clerk/nextjs";
 
 export default function NavbarManager() {
   const { isSignedIn } = useAuthContext();
-  return isSignedIn ? <Navbar navigationItems={dataNavigation} /> : null;
-} 
+  // return isSignedIn ? <Navbar navigationItems={dataNavigation} /> : null;
+  return isSignedIn ? (
+    <div>
+      <XNavbar
+        pathHome="/"
+        pathLikes="/likes"
+        pathMatches="/matches"
+        pathSettings="/settings"
+        development={false}
+      />
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  ) : null;
+}
--- frontend/src/components/XNavbar.tsx
+++ frontend/src/components/XNavbar.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { Button } from "./ui/button";
 import { Heart, House, MessageCircle, User } from "lucide-react";
 import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
 
 interface XNavbarProps {
   pathHome: string;
   pathLikes: string;
   pathMatches: string;
   pathSettings: string;
+  development?: boolean;
 }
 
-const XNavbar = ({ pathHome, pathLikes, pathMatches, pathSettings }: XNavbarProps) => {
+const XNavbar = ({ pathHome, pathLikes, pathMatches, pathSettings, development }: XNavbarProps) => {
   return (
-    <div className="bg-black text-white fixed bottom-0 left-0 w-full flex items-center justify-center space-x-10 p-2">
+    <div className="bg-black text-white fixed bottom-0 left-0 w-full flex items-center justify-center space-x-6 p-2">
       {/* Feed */}
       <Button variant="ghost">
         <Link href={pathHome}>
@@ -40,6 +42,8 @@ const XNavbar = ({ pathHome, pathLikes, pathMatches, pathSettings }: XNavbarProp
           <User />
         </Link>
       </Button>
+
+      {!development && <UserButton afterSignOutUrl="/" />}
     </div>
   );
 };
