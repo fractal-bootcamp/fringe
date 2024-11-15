f33e8c54602a34178f475087bcd47edc948dd2e9
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-15 16:24:20 -0500

    Create profile pages for all users

--- backend/routes/routesUser.ts
+++ backend/routes/routesUser.ts
@@ -1,14 +1,22 @@
 import express from "express";
-import { getAllUsers, getUserById, updateUserProfile, getSignedPhotoUrl, updateUserPhoto, createUser } from "../controllers/controllerUser";
+import {
+  getAllUsers,
+  getUserById,
+  updateUserProfile,
+  getSignedPhotoUrl,
+  updateUserPhoto,
+  createUser,
+  getUser,
+} from "../controllers/controllerUser";
 import upload from "../middleware/upload";
-    
-const router = express.Router();
 
+const router = express.Router();
 
 router.get("/getAllUsers", getAllUsers);
 router.get("/getCurrentUser", getUserById);
+router.post("/getUser", getUser);
 router.post("/updateProfile", updateUserProfile);
-router.post("/updatePhoto", upload.single('photo'), updateUserPhoto);
+router.post("/updatePhoto", upload.single("photo"), updateUserPhoto);
 router.get("/photos/:photoId", getSignedPhotoUrl);
 router.post("/createUser", createUser);
 
